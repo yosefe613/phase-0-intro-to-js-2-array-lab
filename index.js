d64594ff30f7fb1cb1f307aca5a061f7d001b99b
Yosef Ezra <ezzy124@yahoo.com>
2022-05-24 21:44:44 -0400

    commit tree 22af83e913e0c689d6403ca79c5557a48f8579fa

--- index.js
+++ index.js
@@ -1,1 +1,30 @@
 // Write your solution here!
+const cats = ["Milo", "Otis", "Garfield"];
+function destructivelyAppendCat(name) {
+    cats.push(name) 
+}
+function destructivelyPrependCat(name) {
+    cats.unshift(name)
+}
+function destructivelyRemoveLastCat(name) {
+    cats.pop()
+}
+function destructivelyRemoveFirstCat () {
+    cats.shift()
+}
+function removeLastCat() {
+    cats.pop()
+}
+const copyCats = [...cats, "Broom"];
+function appendCat(name) {
+return copyCats;
+}
+function prependCat(name) {
+    return prependCat =[name, ...cats];
+}   
+function removeLastCat(name) {
+    return cats.slice(0, cats.length -1);
+ } 
+ function removeFirstCat(name) {
+     return cats.slice(1);
+ }
